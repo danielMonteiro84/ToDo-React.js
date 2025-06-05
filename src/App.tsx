8f187081f1da00b6576f8dfff5eeae49feba40c6
daniel <danielchsan@gmail.com>
2025-06-05 15:31:25 -0300

    adicionando `CVA` ao component `text.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Text from "./components/text";
+
 export default function App() {
-  return <div className="text-pink-dark">Ola Mundo!</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Text variant="body-sm-bold">Olá Mundo!</Text>
+      <Text variant="body-md">Olá Mundo!</Text>
+      <Text variant="body-md-bold">Olá Mundo!</Text>
+    </div>
+  );
 }
