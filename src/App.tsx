7ee0f91c1523605e1c3061ef022f62c939e910a3
daniel <danielchsan@gmail.com>
2025-06-06 09:45:21 -0300

    adicionando component `icons.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import CheckIcon from "./assets/icons/check.svg?react";
+import PencilIcon from "./assets/icons/pencil.svg?react";
+import PlusIcon from "./assets/icons/plus.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
+import XIcon from "./assets/icons/x.svg?react";
+import Icon from "./components/icons";
 
 export default function App() {
   return (
-    <div className="flex flex-col gap-2">
-      <Text variant="body-sm-bold">Olá Mundo!</Text>
-      <Text variant="body-md">Olá Mundo!</Text>
-      <Text variant="body-md-bold">Olá Mundo!</Text>
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold">Olá Mundo!</Text>
+        <Text variant="body-md">Olá Mundo!</Text>
+        <Text variant="body-md-bold">Olá Mundo!</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+        <Icon svg={CheckIcon} />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SpinnerIcon} animate />
+        <Icon svg={XIcon} />
+      </div>
     </div>
   );
 }
