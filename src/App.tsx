6695165f8925a2a15dd31cfa4a3f069d6741646a
daniel <danielchsan@gmail.com>
2025-06-12 10:31:30 -0300

    adicionando component `container.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -12,44 +12,47 @@ import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
 import Card from "./components/card";
+import Container from "./components/container";
 
 export default function App() {
   return (
-    <div className="grid gap-4">
-      <div className="flex flex-col gap-2">
-        <Text variant="body-sm-bold">Olá Mundo!</Text>
-        <Text variant="body-md">Olá Mundo!</Text>
-        <Text variant="body-md-bold">Olá Mundo!</Text>
+    <Container>
+      <div className="grid gap-4">
+        <div className="flex flex-col gap-2">
+          <Text variant="body-sm-bold">Olá Mundo!</Text>
+          <Text variant="body-md">Olá Mundo!</Text>
+          <Text variant="body-md-bold">Olá Mundo!</Text>
+        </div>
+        <div className="flex gap-1">
+          <Icon svg={TrashIcon} className="fill-pink-base" />
+          <Icon svg={CheckIcon} />
+          <Icon svg={PencilIcon} />
+          <Icon svg={PlusIcon} />
+          <Icon svg={SpinnerIcon} animate />
+          <Icon svg={XIcon} />
+        </div>
+        <div>
+          <Bagde variant="secundary">5</Bagde>
+          <Bagde variant="primary">2 de 5</Bagde>
+        </div>
+        <div>
+          <Button icon={PlusIcon}>Nova Tarefa</Button>
+        </div>
+        <div className="flex gap-1">
+          <ButtonIcon icon={TrashIcon} />
+          <ButtonIcon icon={TrashIcon} variant="secundary" />
+          <ButtonIcon icon={TrashIcon} variant="tertiary" />
+        </div>
+        <div>
+          <InputText />
+        </div>
+        <div>
+          <InputCheckbox />
+        </div>
+        <div>
+          <Card size="md">Olá Mundo</Card>
+        </div>
       </div>
-      <div className="flex gap-1">
-        <Icon svg={TrashIcon} className="fill-pink-base" />
-        <Icon svg={CheckIcon} />
-        <Icon svg={PencilIcon} />
-        <Icon svg={PlusIcon} />
-        <Icon svg={SpinnerIcon} animate />
-        <Icon svg={XIcon} />
-      </div>
-      <div>
-        <Bagde variant="secundary">5</Bagde>
-        <Bagde variant="primary">2 de 5</Bagde>
-      </div>
-      <div>
-        <Button icon={PlusIcon}>Nova Tarefa</Button>
-      </div>
-      <div className="flex gap-1">
-        <ButtonIcon icon={TrashIcon} />
-        <ButtonIcon icon={TrashIcon} variant="secundary" />
-        <ButtonIcon icon={TrashIcon} variant="tertiary" />
-      </div>
-      <div>
-        <InputText />
-      </div>
-      <div>
-        <InputCheckbox />
-      </div>
-      <div>
-        <Card size="md">Olá Mundo</Card>
-      </div>
-    </div>
+    </Container>
   );
 }
